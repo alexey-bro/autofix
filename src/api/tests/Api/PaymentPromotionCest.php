90932115d8d23a219b5676a88bee31e08f48dcbb
Alexey Bro <letters.graig@gmail.com>
2026-04-20 23:33:25 +0300

    codeception init

--- src/api/tests/Api/PaymentPromotionCest.php
+++ src/api/tests/Api/PaymentPromotionCest.php
@@ -18,18 +18,48 @@ final class PaymentPromotionCest
     }
 
     // All `public` methods will be executed as tests.
-    public function tryToTest(ApiTester $I): void
+    public function tryCreatePromotion(ApiTester $I): void
     {
         // Write your test content here.
         $I->sendPost('functions/payment-promotion', [
-            'promotion_id' => 1, // подставь реальный ID
+            "userId" => 51,
+            "title" => "Скидка 99%",
+            "description" => "Дорожная 46",
+            "validUntil" => "До 2030",
+            "phoneNumber" => "+79609998877",
+            "conditions" => "Необходимо налить воды в стакан",
+            "imageUrl" => "imageUrl",
+            "amount" =>  13
         ]);
 
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
         $I->seeResponseContainsJson([
-//            'success' => true,
-            'code' => 141,
+            'success' => true,
         ]);
+
+//        // Проверяем, что ключ promotionId существует и его значение - число
+//        $I->seeResponseJsonMatchesJsonPath('$.promotionId');
+//
+//        // Проверяем, что значение - целое число (дополнительная проверка)
+//        $response = $I->grabResponse();
+//        $data = json_decode($response, true);
+//        $I->assertIsInt($data['promotionId']);
     }
+
+    // All `public` methods will be executed as tests.
+//    public function tryToTest(ApiTester $I): void
+//    {
+//        // Write your test content here.
+//        $I->sendPost('functions/payment-promotion', [
+//            'promotion_id' => 1, // подставь реальный ID
+//        ]);
+//
+//        $I->seeResponseCodeIs(200);
+//        $I->seeResponseIsJson();
+//        $I->seeResponseContainsJson([
+////            'success' => true,
+//            'code' => 141,
+//        ]);
+//    }
 }
