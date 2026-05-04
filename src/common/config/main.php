b283d15980351ec0cdd03a4b475a51937547ba7f
Alexey Bro <letters.graig@gmail.com>
2026-05-04 17:54:26 +0300

    install and settings queue

--- src/common/config/main.php
+++ src/common/config/main.php
@@ -13,11 +13,28 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=docker_db_name;dbname=autofix',
+            'username' => '1111',
+            'password' => '1111',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
+        ],
+        'queue' => [
+            'class' => \yii\queue\db\Queue::class,
+            'db' => 'db', // Database connection component
+            'tableName' => '{{%queue}}', // Table storing the queue
+            'channel' => 'default', // Queue channel
+            'mutex' => \yii\mutex\MysqlMutex::class, // Avoid race conditions
+            'as log' => \yii\queue\LogBehavior::class,
+            'ttr' => 2 * 60, // Максимальное время выполнения задания
+            'attempts' => 3, // Максимальное кол-во попыток
         ],
     ],
 ];
