08b9789b86e1ad1c025dd16671b0d2fceaadf054
Alexey Bro <letters.graig@gmail.com>
2026-06-14 20:25:04 +0300

    added fcm token

--- src/api/modules/v1/controllers/FunctionsController.php
+++ src/api/modules/v1/controllers/FunctionsController.php
@@ -112,7 +112,7 @@ class FunctionsController extends BaseController
 
     //Например: PATCH /api/fcm_token_for_user/<id>/
     #[OA\Post(
-        path: '/function/fcm-token',
+        path: '/functions/fcm-token',
         tags: ['Common'],
         requestBody: new OA\RequestBody(
             description: 'Fcm Token',
