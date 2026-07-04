b701da64c4e78183078f50a3f990619539f07d70
Alexey Bro <letters.graig@gmail.com>
2026-07-04 19:10:48 +0300

    fix user

--- src/common/models/User.php
+++ src/common/models/User.php
@@ -159,9 +159,9 @@ class User extends ActiveRecord implements IdentityInterface
             //Теперь будет несколько userProfile, если нет ни одного, то приложение должно работать и без него
 
 //            Yii::info("Создан новый пользователь: {$this->id}");
-//            $UserProfile = new UserProfile();
-//            $UserProfile->user_id = $this->id;
-//            $UserProfile->save();
+            $UserProfile = new UserProfile();
+            $UserProfile->user_id = $this->id;
+            $UserProfile->save();
 
             // Например: отправка письма, создание профиля, логирование
             // Profile::create(['user_id' => $this->id]);
